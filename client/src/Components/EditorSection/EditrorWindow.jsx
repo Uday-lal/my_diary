6fdccc2859ed85c9573aaef66f7030fc0674e65f
Uday lal <uday.lal@mrndigital.in>
2024-01-26 22:08:37 +0530

    Add page.jsx

--- client/src/Components/EditorSection/EditrorWindow.jsx
+++ client/src/Components/EditorSection/EditrorWindow.jsx
@@ -1,9 +1,14 @@
+import Page from "./Page.jsx";
 import "../../styles/EditorWindow.css";
 
 function EditorWindow(props) {
     return (
         <>
-            <div className="editor_window"></div>
+            <div className="editor_window">
+                <div className="window_page_container">
+                    <Page />
+                </div>
+            </div>
         </>
     );
 }
---
+++ client/src/Components/EditorSection/Page.jsx
@@ -0,0 +1,11 @@
+import "../../styles/Page.css";
+
+function Page(props) {
+    return (
+        <>
+            <div className="editor_page"></div>
+        </>
+    );
+}
+
+export default Page;
